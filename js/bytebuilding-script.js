4c7b8771a7d0aae375b4b8f7fc2f581efc5fcebb
Daniel Mena <slowpoke@Daniels-Air.lan>
2022-06-06 22:06:18 -0700

    updated css

--- js/bytebuilding-script.js
+++ js/bytebuilding-script.js
@@ -1,31 +1,3 @@
-// Slideshow
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("bb-slideshow-image");
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" bb-white", "");
-  }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " bb-white";
-}
-
 function sendContactEmail() {
-  alert('dsdsd');
+  window.open('mailto:test@example.com?subject=subject&body=body');
 }
