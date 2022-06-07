ee08b42db2dc3dcb9fe6f5adda025c7936b471a0
Daniel Mena <slowpoke@Daniels-Air.lan>
2022-06-07 04:48:16 -0700

    updated css

--- js/bytebuilding-script.js
+++ js/bytebuilding-script.js
@@ -7,4 +7,9 @@ function sendContactEmail() {
    message.value = message.value + '%0D%0A' + ',' + name.value;
 
   window.open('mailto:'+email.value+'?subject='+subject.value+'&body='+message.value);
+
+  document.getElementById('bb-contact-name').value = '';
+  document.getElementById('bb-contact-email').value = '';
+  document.getElementById('bb-contact-subject').value = '';
+  document.getElementById('bb-contact-message').value = '';
 }
