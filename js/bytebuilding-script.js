37c250a44a7e43c26275b5889186ce422f558e3d
Daniel Mena <slowpoke@Daniels-Air.lan>
2022-06-07 04:56:30 -0700

    updated css

--- js/bytebuilding-script.js
+++ js/bytebuilding-script.js
@@ -1,12 +1,13 @@
 function sendContactEmail() {
   var name = document.getElementById('bb-contact-name');
-  var email = document.getElementById('bb-contact-email');
   var subject = document.getElementById('bb-contact-subject');
   var message = document.getElementById('bb-contact-message');
 
+  var email = 'bytebuilding@gmail.com';
+
    message.value = message.value + '%0D%0A%0D%0A' + ',' + name.value;
 
-  window.open('mailto:'+email.value+'?subject='+subject.value+'&body='+message.value);
+  window.open('mailto:'+email+'?subject='+subject.value+'&body='+message.value);
 
   document.getElementById('bb-contact-name').value = '';
   document.getElementById('bb-contact-email').value = '';
